2cdf4341e8d3b1820e8c489b7a70d8a6a2141324
ghost <ghost@DESKTOP-74KI6DB>
2021-05-27 17:49:12 +0900

    initial commit

---
+++ VGC_Project/DB작성.sql
@@ -0,0 +1,38 @@
+SET AUTOCOMMIT = FALSE;
+
+CREATE TABLE if not exists user(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    user_Id VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    birth_Date VARCHAR(8) NOT NULL,
+    phone VARCHAR(11) NOT NULL
+);
+CREATE TABLE if not exists movie(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    running_Time INT NOT NULL
+);
+
+CREATE TABLE if not exists seat(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    seat_Type VARCHAR(5),
+    seat_Row INT, 
+    seat_Col INT
+);
+
+CREATE TABLE if not exists reserve(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_Id VARCHAR(30) NOT NULL,
+    movie_Id INT NOT NULL,
+    reserve_Date VARCHAR(40) NOT NULL,
+    reserve_Time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    reserve_Cnt INT NOT NULL,
+    seat VARCHAR(50) NOT NULL,
+    FOREIGN KEY(movie_Id) REFERENCES movie(id)
+);
+
+INSERT INTO seat VALUES (NULL,'A',5,5);
+
+
+COMMIT;
